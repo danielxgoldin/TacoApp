af4df4e1e68200f1107462daf8b40a80329245e6
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-06-22 14:38:07 -0400

    added script for materialize js and added js code for responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
+
+// materialize navbar code
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
 $(document).ready(function () {
-    $(".button").on("click", function() {
-        var scheduleText = $(this).siblings(".form").val();
-        var timeId = $(this).parent().attr("id");
-        localStorage.setItem();
-        console.log();
-        });
+  $(".button").on("click", function() {
+      var scheduleText = $(this).siblings(".form").val();
+      var timeId = $(this).parent().attr("id");
+      localStorage.setItem();
+      console.log();
+      });
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-              center: {},
-              zoom: 8,
-            });
-          }
-          
-          window.initMap = initMap;
+      function initMap() {
+          map = new google.maps.Map(document.getElementById("map"), {
+            center: {},
+            zoom: 8,
+          });
+        }
+        
+        window.initMap = initMap;
 
-          map = new google.maps.Map(document.getElementById("map")
+        map = new google.maps.Map(document.getElementById("map")  )})
