8431769965fcad0a707c749f5cebf935ef169cda
danielxgoldin <104451480+danielxgoldin@users.noreply.github.com>
2022-06-21 17:18:09 -0700

    script

---
+++ TacoApp/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".button").on("click", function() {
+        var scheduleText = $(this).siblings(".form").val();
+        var timeId = $(this).parent().attr("id");
+        localStorage.setItem();
+        console.log();
+        });
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+              center: {},
+              zoom: 8,
+            });
+          }
+          
+          window.initMap = initMap;
+
+          map = new google.maps.Map(document.getElementById("map")
